ec76d8205b2c38c9e7357269a9a7cc6499055cc4
toughchb <ggang2000@gmail.com>
2023-11-06 12:35:06 +0900

    chapter05

--- src/Node.h
+++ src/Node.h
@@ -15,29 +15,29 @@ struct Program {
 };
 
 struct Statement {
-  virtual auto interpret()->void = 0;
+  virtual auto generate()->void = 0;
 };
 
 struct Expression {
-  virtual auto interpret()->any = 0;
+  virtual auto generate()->void = 0;
 };
 
 struct Function: Statement {
   string name;
   vector<string> parameters;
   vector<Statement*> block;
-  auto interpret()->void;
+  auto generate()->void;
 };
 
 struct Variable: Statement {
   string name;
   Expression* expression;
-  auto interpret()->void;
+  auto generate()->void;
 };
 
 struct Return: Statement {
   Expression* expression;
-  auto interpret()->void;
+  auto generate()->void;
 };
 
 struct For: Statement {
@@ -45,122 +45,122 @@ struct For: Statement {
   Expression* condition;
   Expression* expression;
   vector<Statement*> block;
-  auto interpret()->void;
+  auto generate()->void;
 };
 
 struct Break: Statement {
-  auto interpret()->void;
+  auto generate()->void;
 };
 
 struct Continue: Statement {
-  auto interpret()->void;
+  auto generate()->void;
 };
 
 struct If: Statement {
   vector<Expression*> conditions;
   vector<vector<Statement*>> blocks;
   vector<Statement*> elseBlock;
-  auto interpret()->void;
+  auto generate()->void;
 };
 
 struct Print: Statement {
   bool lineFeed = false;
   vector<Expression*> arguments;
-  auto interpret()->void;
+  auto generate()->void;
 };
 
 struct ExpressionStatement: Statement {
   Expression* expression;
-  auto interpret()->void;
+  auto generate()->void;
 };
 
 struct Or: Expression {
   Expression* lhs;
   Expression* rhs;
-  auto interpret()->any;
+  auto generate()->void;
 };
 
 struct And: Expression {
   Expression* lhs;
   Expression* rhs;
-  auto interpret()->any;
+  auto generate()->void;
 };
 
 struct Relational: Expression {
   Kind kind;
   Expression* lhs;
   Expression* rhs;
-  auto interpret()->any;
+  auto generate()->void;
 };
 
 struct Arithmetic: Expression {
   Kind kind;
   Expression* lhs;
   Expression* rhs;
-  auto interpret()->any;
+  auto generate()->void;
 };
 
 struct Unary: Expression {
   Kind kind;
   Expression* sub;
-  auto interpret()->any;
+  auto generate()->void;
 };
 
 struct Call: Expression {
   Expression* sub;
   vector<Expression*> arguments;
-  auto interpret()->any;
+  auto generate()->void;
 };
 
 struct GetElement: Expression {
   Expression* sub;
   Expression* index;
-  auto interpret()->any;
+  auto generate()->void;
 };
 
 struct SetElement: Expression {
   Expression* sub;
   Expression* index;
   Expression* value;
-  auto interpret()->any;
+  auto generate()->void;
 };
 
 struct GetVariable: Expression {
   string name;
-  auto interpret()->any;
+  auto generate()->void;
 };
 
 struct SetVariable: Expression {
   string name;
   Expression* value;
-  auto interpret()->any;
+  auto generate()->void;
 };
 
 struct NullLiteral: Expression {
-  auto interpret()->any;
+  auto generate()->void;
 };
 
 struct BooleanLiteral: Expression {
   bool value = false;
-  auto interpret()->any;
+  auto generate()->void;
 };
 
 struct NumberLiteral: Expression {
   double value = 0.0;
-  auto interpret()->any;
+  auto generate()->void;
 };
 
 struct StringLiteral: Expression {
   string value;
-  auto interpret()->any;
+  auto generate()->void;
 };
 
 struct ArrayLiteral: Expression {
   vector<Expression*> values;
-  auto interpret()->any;
+  auto generate()->void;
 };
 
 struct MapLiteral: Expression {
   map<string, Expression*> values;
-  auto interpret()->any;
+  auto generate()->void;
 };
